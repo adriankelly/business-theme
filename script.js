95cb08af1d549768251d9282aa586f8fcf97404a
Adrian Kelly <adriankellyweb@gmail.com>
2015-02-16 13:59:51 +1100

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.top').fadeIn(duration);
+        } else {
+            $('.top').fadeOut(duration);
+        }
+    });
+    
+    $('.top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
